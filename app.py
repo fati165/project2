74618f1e0e48e9dbfacd20d07b1aacabc873f736
fati165 <gdonato38@gmail.com>
2020-11-04 21:28:23 -0800

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import datetime as dt
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/crime_data"
 mongo = PyMongo(app)
 
 @app.route("/")
