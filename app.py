15716ba75a06da03cfaef76c2447055f938f138f
unknown <gilliskat@aol.com>
2020-11-11 20:55:06 -0800

    flask app api and geoJson map layer

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response, url_for
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import json
 
 import numpy as np
 import datetime as dt
@@ -13,15 +14,20 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/crime_data"
 mongo = PyMongo(app)
 
 @app.route("/")
-
 def homepage(): 
     # Find data
-    crimeinfo = mongo.db.crime_data.find_one()
+    # crimeinfo = mongo.db.crime_data.find_one()
     #marsinfo = mongo.db.marsdata.find_one()
 
     # Return template and data
-    return render_template("index.html", crimes_ = crimeinfo )
+    return render_template("index.html")
     #return render_template("index.html", mars_=marsinfo)
+
+@app.route("/api/data")
+def data():
+    crimeinfo = list(mongo.db.project2.find())
+    return Response(json.dumps(crimeinfo,default=str),mimetype="application/json")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
